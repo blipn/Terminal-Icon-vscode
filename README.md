2fa5eca94d11127a0d34cc7fd30fa49011aa6418
blipn <oblipn@gmail.com>
2023-04-28 23:30:20 +0200

    Create Extension

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Terminal-Icon-vscode
+# terminal-icon README
+
+Terminal Icon for VSCode
+
+## Features
+
+Display a tiny terminal icon at the right bottom, in the VScode Status Bar.
+
+\!\[feature\]\(images/screen.jpg\)
+
+## Known Issues
+
+*none
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of terminal-icon
+
+**Enjoy!**
+
+Made by [Blipn](https://blipn.fr) ☀ 
---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+import * as vscode from 'vscode';
+import * as os from 'os';
+import * as path from 'path';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "terminal-icon" is now active!');
+
+	let toggle = vscode.commands.registerCommand('terminal.toggle', () => {
+		vscode.commands.executeCommand("workbench.action.togglePanel");
+	});
+	context.subscriptions.push(toggle);
+
+	let toggleTerminalStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	toggleTerminalStatusBarItem.command = "terminal.toggle";
+	toggleTerminalStatusBarItem.text = " $(terminal) ";
+	toggleTerminalStatusBarItem.tooltip = "Toggle Integrated Terminal";
+	toggleTerminalStatusBarItem.show();
+}
+
+export function deactivate() {}
